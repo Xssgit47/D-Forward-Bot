f2ad1e5594fa97ad1e8c0b3f9648b0f6164e4e55
Viraaj Patel <xarteastmin@gmail.com>
2025-03-29 08:30:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ class Config:
     API_HASH = environ.get("API_HASH", "e93479e4b3eae090bf90a978b14ffc09")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "")
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://viraaj35:viraajmongo@danger35.wgk98.mongodb.net/?retryWrites=true&w=majority&appName=Danger35")
     DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
-    BOT_OWNER = int(environ.get("BOT_OWNER", ""))
+    BOT_OWNER = int(environ.get("BOT_OWNER", "7345260405"))
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot
