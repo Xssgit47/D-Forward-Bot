755920d9e37b07a58a74752d8df56c05d7ede3e9
Viraaj Patel <xarteastmin@gmail.com>
2025-03-29 08:29:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,11 @@
-# Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Ask Doubt on telegram @fnxdanger
 
 from os import environ 
 
 class Config:
-    API_ID = int(environ.get("API_ID", ""))
-    API_HASH = environ.get("API_HASH", "")
+    API_ID = int(environ.get("API_ID", "23418117"))
+    API_HASH = environ.get("API_HASH", "e93479e4b3eae090bf90a978b14ffc09")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
     DATABASE_URI = environ.get("DATABASE_URI", "")
@@ -14,7 +13,7 @@ class Config:
     BOT_OWNER = int(environ.get("BOT_OWNER", ""))
 
 # Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Subscribe YouTube Channel For Amazing Bot
 # Ask Doubt on telegram @KingVJ01
 
 class temp(object): 
